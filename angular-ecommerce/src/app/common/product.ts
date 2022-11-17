6318183fcfcb210b783f7c61b0316e1a8a055da8
Ayoub Ltifi <ayoub.ltifi1@gmail.com>
2022-11-17 16:00:03 +0100

    create the front end files

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+
+    constructor(
+        public sku: string,
+        public name: string,
+        public description: string,
+        public unitPrice: string,
+        public imageUrl: string,
+        public active: string,
+        public unitsInStock: string,
+        public dateCreated: string,
+        public lastupdated: string,
+    ){
+
+    }
+}
