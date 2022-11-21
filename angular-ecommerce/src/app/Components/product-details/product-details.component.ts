e95c5e506c224a0c91f181e43def7f86350c7570
Ayoub Ltifi <ayoub.ltifi1@gmail.com>
2022-11-21 16:33:41 +0100

    add products to cart from product detail

--- angular-ecommerce/src/app/Components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/Components/product-details/product-details.component.ts
@@ -2,33 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
   product!: Product;
-  constructor(private productService: ProductService,
-    private route: ActivatedRoute){
-
-  }
+  constructor(
+    private productService: ProductService,
+    private cartservice: CartService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.route.paramMap.subscribe(()=>{
+    this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
-    })
+    });
   }
   handleProductDetails() {
-    
     //get the id param and convert it to a number using + symbol
     const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
 
-    this.productService.getProduct(theProductId).subscribe(
-      data => {
-        this.product = data;
-      }
-    )
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
   }
-
+  addToCart() {
+    let cartItem: CartItem = new CartItem(this.product);
+    this.cartservice.addToCart(cartItem);
+  }
 }
--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductCategoryMenuComponent } from './Components/product-category-menu
 import { SearchComponent } from './Components/search/search.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
   {path: 'products/:id',component: ProductDetailsComponent},
@@ -29,7 +30,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
