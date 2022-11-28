f8c2d01454d86f7856cd698f6f2682b34be97897
Ayoub Ltifi <ayoub.ltifi1@gmail.com>
2022-11-28 10:11:14 +0100

    disable submit button to prevent multiple payments

--- angular-ecommerce/src/app/Components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/Components/checkout/checkout.component.ts
@@ -46,6 +46,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = '';
 
+  isDisabled: boolean = false;
+
   constructor(
     private luvToShopFormService: Luv2shopFormService,
     private cartService: CartService,
@@ -331,6 +333,7 @@ export class CheckoutComponent implements OnInit {
     //compute payment
     this.paymentInfo.amount = Math.floor(this.totalPrice * 100);
     this.paymentInfo.currency = 'USD';
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     //if the form is valid then
     // -create payment intent
@@ -341,6 +344,7 @@ export class CheckoutComponent implements OnInit {
       !this.checkoutFormGroup.invalid &&
       this.displayError.textContent === ''
     ) {
+      this.isDisabled = true;
       this.checkoutService
         .createPaymentIntent(this.paymentInfo)
         .subscribe((paymentIntentResponse) => {
@@ -350,6 +354,17 @@ export class CheckoutComponent implements OnInit {
               {
                 payment_method: {
                   card: this.cardElement,
+                  billing_details: {
+                    email: purchase.customer.email,
+                    name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                    address: {
+                      line1: purchase.billingAddress.street,
+                      city: purchase.billingAddress.city,
+                      state: purchase.billingAddress.state,
+                      postal_code: purchase.billingAddress.zipCode,
+                      country: this.billingAddressCountry.value.code,
+                    },
+                  },
                 },
               },
               { handleActions: false }
@@ -358,16 +373,20 @@ export class CheckoutComponent implements OnInit {
               if (result.error) {
                 //inform the customer there is an error
                 alert(`There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               } else {
+                // call rest api via checkout service
                 this.checkoutService.placeOrder(purchase).subscribe({
                   next: (response: any) => {
                     alert(
                       `Your order has been recieved.\nOrder tracking number: ${response.orderTrackingNumber}`
                     );
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: (err: any) => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   },
                 });
               }
@@ -384,6 +403,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     // reset the form
     this.checkoutFormGroup.reset();
